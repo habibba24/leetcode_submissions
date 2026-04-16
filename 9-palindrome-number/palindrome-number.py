99276b2874cc9c1da906961a9f4b77a85c32c54a
habiba wael <hapipaweal9@gmail.com>
2026-04-16 15:58:26 +0200

    Time: 5 ms (71.57%) | Memory: 19.3 MB (62.24%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        original = x
+        reversed_number = 0
+
+        while x > 0 :
+            digit = x % 10
+            reversed_number = reversed_number * 10 + digit
+            x=x//10
+
+        return original == reversed_number
+
+x = 121
+sol = Solution()
+print(sol.isPalindrome(x))
+    
